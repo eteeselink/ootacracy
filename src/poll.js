c8db408e6e9aa54f927a36e323a43d2c109e72ce
Patel <p.patel@tue.nl>
2018-11-15 16:09:44 +0100

    poll page stage 1

--- src/poll.js
+++ src/poll.js
@@ -1,5 +1,4 @@
 import {Results} from './Results.js';
-import {results} from './results-mock.js'
 
 export class Poll {
     /**
@@ -9,11 +8,32 @@ export class Poll {
         this.element = element;
     }
 
-    render(name) {
-        this.element.innerHTML = `
-            POLL PAGE
+    render(questions) {
+        var results=[];
+        this.element.innerHTML="<h1>POLL PAGE<h1>";
 
-            <button id="btn-next">Next</button>
+        for(var question of questions){
+            this.element.innerHTML+= `<h2>${question.question}<h2>`;
+            for(const option of question.options){
+                this.element.innerHTML+= `<input type="radio" name="${question.question}" value="${question.question}"/> ${option} <br>`
+            }
+
+        }
+
+        console.log(questions)
+        for (var i = 0; i < questions.length; i++) { 
+            var obj={}
+            obj.type=questions[i].type;
+            obj.question=questions[0].question
+            obj.results=[]
+            results.push(obj)
+        }
+
+        //const stringhkg=`<button id="btn-next">Submit</button>`;
+
+        this.element.innerHTML += `<br>
+            <button id="btn-next">Submit</button>
+            <button id="btn-next">Final Results</button>
         `;
 
         this.element.querySelector("#btn-next").addEventListener("click", ev => {
@@ -24,7 +44,7 @@ export class Poll {
             ev.preventDefault();
 
             const page = new Results(this.element);
-            page.render(results);
+            page.render("results should be here");
         })
     }
 }
